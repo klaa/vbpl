d8a18065cb346f3ab1693560f970bdd554ee2029
Nguyen Huu Khanh <nguyenkhanh87@gmail.com>
2020-04-04 01:06:30 +0700

    Init starter

---
+++ resources/lang/en/home.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'about'=> 'About us',
+    'journey_manage' => 'Journey Management',
+    'journey_list'  => 'Journey list',
+    'journey_create' => 'Create new journey',
+    'car_manage' => 'Car management',
+    'car_list'  => 'Car list',
+    'car_create' => 'Create new car',
+    'driver_manage' => 'Driver management',
+    'driver_list'  => 'Driver list',
+    'driver_create' => 'Create new driver',
+];
